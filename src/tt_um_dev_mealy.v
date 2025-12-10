1a667a689bdeb660f0691deed7898438a433181d
dev-d0g <dhart0196@gmail.com>
2025-12-10 18:25:31 -0500

    Update tt_um_dev_mealy.v

--- src/tt_um_dev_mealy.v
+++ src/tt_um_dev_mealy.v
@@ -1,5 +1,5 @@
 `default_nettype none
-module tt_um_islam_ihfaz_mealy (
+module tt_um_dev_mealy (
 input wire [7:0] ui_in, // Dedicated inputs
 output wire [7:0] uo_out, // Dedicated outputs
 input wire [7:0] uio_in, // IOs: Input path
